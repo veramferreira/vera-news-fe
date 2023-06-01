48a7df2241a9093661295838b6dbfd4448794581
Vera Ferreira <verammferreira@gmail.com>
2023-06-01 09:38:28 +0100

    add article votes functionality - ticket 7

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,12 +3,15 @@ import { useParams } from "react-router-dom";
 import { fetchArticleById } from "../utils";
 import BeatLoader from "react-spinners/BeatLoader";
 import Comments from "./Comments";
+import InsertEmoticonOutlinedIcon from "@mui/icons-material/InsertEmoticonOutlined";
+import MoodBadOutlinedIcon from "@mui/icons-material/MoodBadOutlined";
 
 export default function ArticleCard() {
   const [article, setArticle] = useState({});
   const { article_id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   let [color, setColor] = useState("#ffffff");
+  const [voted, setVoted] = useState(0);
 
   useEffect(() => {
     fetchArticleById(article_id)
@@ -35,6 +38,30 @@ export default function ArticleCard() {
     );
   }
 
+  function handleUpVote() {
+    setVoted((currVotes) => {
+      const newVote = currVotes + 1;
+      setArticle((currArticle) => {
+        return { ...currArticle, votes: newVote };
+      });
+      return newVote;
+    });
+  }
+
+  function handleDownVote() {
+    setVoted((currVotes) => {
+      if (currVotes !== 0) {
+        const newVote = currVotes - 1;
+        setArticle((currArticle) => {
+          return { ...currArticle, votes: newVote };
+        });
+        return newVote;
+      } else {
+        return currVotes;
+      }
+    });
+  }
+
   const { title, author, body, created_at, topic, votes, article_img_url } =
     article;
 
@@ -43,13 +70,27 @@ export default function ArticleCard() {
       <article className="article">
         <h2 className="article--title">{title}</h2>
         <p className="article--topic">• {topic} •</p>
+        <p className="article--author">posted by: {author}</p>
         <img
           className="article--img"
           src={article_img_url}
           alt={`something related to ${title}`}
         />
-        <p className="article--body">{body}</p>
-        <p className="article--author">written by: {author}</p>
+        <div className="article--body">
+          <p>{body}</p>
+          <p className="bold">Did you like this article?</p>
+          <div className="article--votes">
+            <InsertEmoticonOutlinedIcon
+              className="vote--emoji upVote"
+              onClick={handleUpVote}
+            />
+            <span className="current-votes bold">{voted}</span>
+            <MoodBadOutlinedIcon
+              className="vote--emoji downVote"
+              onClick={handleDownVote}
+            />
+          </div>
+        </div>
       </article>
       <Comments article_id={article_id} />
     </>
